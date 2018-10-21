e05c23d55b20d554a250f802c976a3fd8fc02978
simonibrahim <43915855+simonibrahim@users.noreply.github.com>
2018-10-21 16:22:42 +0200

    21.10.2018

---
+++ Bsp03/app2.js
@@ -0,0 +1,24 @@
+/*
+let name ='Susi';
+console.log('name =', name, name.constructor);
+*/
+/*
+let hallo = function(str) {
+    str = "Hallo" + str;
+};
+
+let welt = "Welt";
+hallo(welt);
+console.log(welt);
+*/
+let halloObject = function(obj) {
+    obj = null;
+};
+
+let o = {
+    name : 'Welt'
+};
+
+halloObject(o);
+
+console.log('Ausgabe im Main', o);
